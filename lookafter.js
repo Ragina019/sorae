d80026e2b922d413ba30f7db5a746ea11deaf64b
Ragina019 <raginamelin019@gmail.com>
2026-06-11 21:07:30 +0630

    Update comic details in lookafter.js

---
+++ lookafter.js
@@ -0,0 +1,14 @@
+{
+        id: "chapter-1",
+        title: "Look after",
+        author: "EQ san",
+        // The cover image link:
+        cover: "https://imgur.com/undefined.jpg", 
+        description: "Romeance.",
+        // The individual comic page links:
+        pages: [
+            "https://imgur.com/undefined.png", // Page 1
+            "https://i.imgur.com/LMN9012.jpg", // Page 2
+            "https://i.imgur.com/OPQ3456.jpg"  // Page 3
+        ]
+    }
