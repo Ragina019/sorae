3d4a4be0aab326d574c8dcbd4e6081b6174377b6
Ragina019 <raginamelin019@gmail.com>
2026-06-11 20:59:33 +0630

    Add files via upload

---
+++ gemini-code-1781188149268.js
@@ -0,0 +1,14 @@
+{
+        id: "sorae-chapter-1",
+        title: "Sorae Chronicles - Chapter 1",
+        author: "Your_Artist_Name",
+        // The cover image link:
+        cover: "https://i.imgur.com/ABC1234.jpg", 
+        description: "Awesome description here.",
+        // The individual comic page links:
+        pages: [
+            "https://i.imgur.com/XYZ5678.png", // Page 1
+            "https://i.imgur.com/LMN9012.jpg", // Page 2
+            "https://i.imgur.com/OPQ3456.jpg"  // Page 3
+        ]
+    }
